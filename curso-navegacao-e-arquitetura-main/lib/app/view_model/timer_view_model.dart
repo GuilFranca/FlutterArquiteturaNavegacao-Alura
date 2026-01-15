872278d2bf894a9b5a7aaff6f21e0dea0d06baa6
Guilherme França <guilfranca27@gmail.com>
2026-01-15 15:41:11 -0300

    Aula 3 - 03 Criando o Change Notifier

---
+++ curso-navegacao-e-arquitetura-main/lib/app/view_model/timer_view_model.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class TimerViewModel extends ChangeNotifier {
+  bool isPlaying = false;
+
+  Timer? timer;
+  Duration duration = Duration.zero;
+
+  void startTimer(int initialMinutes) {
+    duration = Duration.zero;
+    notifyListeners();
+
+    timer = Timer.periodic(Duration(seconds: 1), (timer) {
+
+      if (duration.inMinutes < initialMinutes) {
+        duration += Duration(seconds: 1);
+        notifyListeners();
+      } else {
+        stopTime();
+      }
+
+    });
+  }
+
+  // Será utilziado a mesma lógica no dispose.
+  void stopTime() {
+    isPlaying = false;
+    timer?.cancel();
+  }
+
+}
